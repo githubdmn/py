cbe91087736a1da8fd6ed3539a7ddd8e39621ddc
githubdmn <dmninbox@gmail.com>
2023-03-18 19:39:29 +0100

    ubuntu docker image

--- dsml/install.sh
+++ dsml/install.sh
@@ -1,8 +1,15 @@
-apk update && apk upgrade --available
-apk add --update python3
-apk add py3-pip
-apk add jupyter-notebook
-apk add ipython
-pip install numpy
-pip install pandas
-
+apt-get update
+apt install build-essential \
+  zlib1g-dev \
+  libncurses5-dev \
+  libgdbm-dev \
+  libnss3-dev \
+  libssl-dev \
+  libreadline-dev \
+  libffi-dev\
+  libsqlite3-dev \
+  wget \
+  libbz2-dev \
+  python3 \
+  python3-pip
+pip3 install numpy pandas 
