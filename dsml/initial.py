fe8cc16dc2c0c1e38c389648e7427677d5ba4ef7
githubdmn <dmninbox@gmail.com>
2023-03-28 18:22:45 +0200

    code formatted

--- dsml/initial.py
+++ dsml/initial.py
@@ -1,5 +1,5 @@
-print("DS and ML")
 import numpy as np
+print("DS and ML")
 
 a = np.array([1, 2, 3])   # Create a rank 1 array
 print(type(a))            # Prints "<class 'numpy.ndarray'>"
@@ -8,6 +8,6 @@ print(a[0], a[1], a[2])   # Prints "1 2 3"
 a[0] = 5                  # Change an element of the array
 print(a)                  # Prints "[5, 2, 3]"
 
-b = np.array([[1,2,3],[4,5,6]])    # Create a rank 2 array
+b = np.array([[1, 2, 3], [4, 5, 6]])    # Create a rank 2 array
 print(b.shape)                     # Prints "(2, 3)"
-print(b[0, 0], b[0, 1], b[1, 0]) 
+print(b[0, 0], b[0, 1], b[1, 0])
