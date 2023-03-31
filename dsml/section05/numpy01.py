eb3ee346ba308dcb3658b98f7af28832430dfa2b
githubdmn <dmninbox@gmail.com>
2023-03-31 12:41:31 +0200

    numpy intro

---
+++ dsml/section05/numpy01.py
@@ -0,0 +1,15 @@
+print("Introduction to numpy")
+
+import numpy as np
+
+alist = [1, 2, 3]
+
+arr = np.array(alist)
+print(arr)
+
+amat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+mat = np.array(amat)
+print(mat)
+
+print(np.arange(0, 9))
+print(np.arange(0, 11, 2))
